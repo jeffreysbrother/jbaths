a5ce20064e6adceefe9536cbbd8a8ebb886187e3
Tim Spinks <tim@ninthlink.com>
2015-01-27 11:18:30 -0800

    [JAC-220] baths search layout updates

--- content-search.php
+++ content-search.php
@@ -4,21 +4,50 @@
  */
 
 global $post;
+
+$attachment = false;
+
+if ( wp_get_attachment_url( $post->ID ) ) {
+	$attachment = true;
+}
+
 ?>
 
-<h2 class="entry-title">
-	<a href="<?php echo get_permalink(); ?>">
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header class="search-result-header">
+	
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="search-result-header">
+		<h2 class="entry-title">
+			<?php if ( $attachment ) { ?>
+				<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">
+			<?php } else { ?>
+				<a href="<?php echo get_permalink(); ?>">
+			<?php } ?>
 				<?php the_title( '', '' ); ?>
-				<?php if ( has_post_thumbnail() ) { ?>
-					<?php the_post_thumbnail('small'); ?>
-				<?php } ?>
-				<?php the_excerpt(); ?>
-				<div class="entry-meta">
-					<?php jbaths_posted_on(); ?>
-				</div><!-- .entry-meta -->
-			</header><!-- .entry-header -->
-		</article><!-- #post-## -->
-	</a>
-</h2>
+			</a>
+		</h2>
+	</header><!-- .entry-header -->
+
+	<section class="search-content">
+		<?php if ( has_post_thumbnail() ) { ?>
+			<?php the_post_thumbnail('small'); ?>
+
+		<?php } else if ( wp_attachment_is_image( $post->ID ) ) { ?>
+			<?php echo wp_get_attachment_image( $post->ID, 'small' ); ?>
+			<p>File type: <?php echo $post->post_mime_type; ?></p>
+
+		<?php } else if ( $post->post_mime_type == 'application/pdf' ) { ?>
+			<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">Download</a>
+			<p>File type: <?php echo $post->post_mime_type; ?></p>
+
+		<?php } ?>
+	</section>
+
+	<section class="search-excerpt">
+		<?php the_excerpt(); ?>
+	</section>
+
+	<div class="entry-meta">
+		<?php jbaths_posted_on(); ?>
+	</div><!-- .entry-meta -->
+
+</article><!-- #post-## -->
