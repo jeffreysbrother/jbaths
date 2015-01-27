589cd030d49f0df9ddffe99cbfa93189cd2e807b
Tim Spinks <tim@ninthlink.com>
2015-01-27 11:29:11 -0800

    [JAC-220] style updates

--- content-search.php
+++ content-search.php
@@ -13,41 +13,41 @@ if ( wp_get_attachment_url( $post->ID ) ) {
 
 ?>
 
-	
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="search-result-header">
-		<h2 class="entry-title">
-			<?php if ( $attachment ) { ?>
-				<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">
-			<?php } else { ?>
-				<a href="<?php echo get_permalink(); ?>">
-			<?php } ?>
-				<?php the_title( '', '' ); ?>
-			</a>
-		</h2>
-	</header><!-- .entry-header -->
+<?php if ( $attachment ) { ?>
+	<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">
+<?php } else { ?>
+	<a href="<?php echo get_permalink(); ?>">
+<?php } ?>
 
-	<section class="search-content">
-		<?php if ( has_post_thumbnail() ) { ?>
-			<?php the_post_thumbnail('small'); ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="search-result-header">
+				<h2 class="entry-title">
+					<?php the_title( '', '' ); ?>
+				</h2>
+			</header><!-- .entry-header -->
 
-		<?php } else if ( wp_attachment_is_image( $post->ID ) ) { ?>
-			<?php echo wp_get_attachment_image( $post->ID, 'small' ); ?>
-			<p>File type: <?php echo $post->post_mime_type; ?></p>
+			<section class="search-content">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<?php the_post_thumbnail('small'); ?>
 
-		<?php } else if ( $post->post_mime_type == 'application/pdf' ) { ?>
-			<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">Download</a>
-			<p>File type: <?php echo $post->post_mime_type; ?></p>
+				<?php } else if ( wp_attachment_is_image( $post->ID ) ) { ?>
+					<?php echo wp_get_attachment_image( $post->ID, 'small' ); ?>
+					<p>File type: <?php echo $post->post_mime_type; ?></p>
 
-		<?php } ?>
-	</section>
+				<?php } else if ( $post->post_mime_type == 'application/pdf' ) { ?>
+					<a href="<?php echo wp_get_attachment_url( $post->ID ); ?>">Download</a>
+					<p>File type: <?php echo $post->post_mime_type; ?></p>
 
-	<section class="search-excerpt">
-		<?php the_excerpt(); ?>
-	</section>
+				<?php } ?>
+			</section>
 
-	<div class="entry-meta">
-		<?php jbaths_posted_on(); ?>
-	</div><!-- .entry-meta -->
+			<section class="search-excerpt">
+				<?php the_excerpt(); ?>
+			</section>
 
-</article><!-- #post-## -->
+			<div class="entry-meta">
+				<?php jbaths_posted_on(); ?>
+			</div><!-- .entry-meta -->
+
+		</article><!-- #post-## -->
+</a>
