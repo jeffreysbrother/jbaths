0b845acb627175f7f673a7e986f7ca683ad4c4d2
Tim Spinks <tim@ninthlink.com>
2015-04-20 14:17:14 -0700

    JAC-471 updated pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -22,11 +22,11 @@ function jbaths_paging_nav() {
 		<div class="nav-links">
 
 			<?php if ( get_next_posts_link() ) : ?>
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'jbaths' ) ); ?></div>
+			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> More Results', 'jbaths' ) ); ?></div>
 			<?php endif; ?>
 
 			<?php if ( get_previous_posts_link() ) : ?>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'jbaths' ) ); ?></div>
+			<div class="nav-next"><?php previous_posts_link( __( 'Previous Results <span class="meta-nav">&rarr;</span>', 'jbaths' ) ); ?></div>
 			<?php endif; ?>
 
 		</div><!-- .nav-links -->
