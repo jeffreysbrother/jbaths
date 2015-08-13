2569b2f079c4e69874dba73d9e8e6740c7d749b6
Tim Spinks <tim@ninthlink.com>
2015-08-13 10:04:41 -0700

    posts search reset for now

--- functions.php
+++ functions.php
@@ -619,8 +619,8 @@ function jbaths_postsperpage($limits) {
 
 function my_searchwp_query_orderby() {
 	global $wpdb;
-	//return "ORDER BY {$wpdb->prefix}posts.post_type DESC, {$wpdb->prefix}posts.post_date DESC";
-	return "ORDER BY FIELD({$wpdb->prefix}posts.post_type, 'bathtubs', 'faucets', 'showers', 'toilets', 'post', 'page'), {$wpdb->prefix}posts.post_date DESC";
+	return "ORDER BY {$wpdb->prefix}posts.post_type DESC, {$wpdb->prefix}posts.post_date DESC";
+	//return "ORDER BY FIELD({$wpdb->prefix}posts.post_type, 'bathtubs', 'faucets', 'showers', 'toilets', 'post', 'page'), {$wpdb->prefix}posts.post_date DESC";
 }
 
 add_filter( 'searchwp_query_orderby', 'my_searchwp_query_orderby' );
