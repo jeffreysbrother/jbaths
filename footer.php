f21ee879287e74e036902abac1af066747f8b4f9
Mitchell Austin <mitchell@ninthlink.com>
2015-11-06 12:51:02 -0800

    Copy/markup update for walk-in popup and temporary removal of the brochure popup

--- footer.php
+++ footer.php
@@ -12,96 +12,6 @@
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/fancybox/jquery.fancybox.pack.js"></script>
 	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
 <?php
-/* Brochure popup on all pages but walk-in, information, brochure-request and warranty-registration */
-if( !is_page( 'walk-in', 'information', 'brochure-request', 'warranty-registration'))
-{
-	?>
-	<script type="text/javascript">
-		jQuery(document).ready(function() {
-			
-			var msLeft,
-			    pagesLeft,
-			    idleTimeoutID,
-			    now;
-
-			if( jQuery.cookie('idlePopped') == "true" ) {
-				return false } //early return skips everything below
-
-			if( jQuery.cookie("idlePagesLeft") && jQuery.cookie("idleCountdown") && jQuery.cookie("idleStamp")) {
-			    //ilding has been previously underway
-				pagesLeft = jQuery.cookie("idlePagesLeft");
-				msLeft = jQuery.cookie("idleCountdown");
-				if(--pagesLeft < 1) idleTimeoutID = setTimeout(idlePopup, msLeft);
-				jQuery.cookie("idlePagesLeft",pagesLeft, {path: '/' }) }
-			else{
-			    //initate idling. config vars here...
-				msLeft = 180000; // total time in milliseconds before the popup will trigger
-				pagesLeft = 3; // number of pages the visitor sees before the popup will trigger
-				now = new Date().getTime();
-				jQuery.cookie("idlePagesLeft",pagesLeft, {path: '/' });
-				jQuery.cookie("idleCountdown",msLeft, {path: '/' });
-				jQuery.cookie("idleStamp",now, {path: '/' }); }
-
-			
-			jQuery(window).unload(function() {
-				var now = new Date().getTime(),
-				    startStamp = jQuery.cookie("idleStamp"),
-				    msDiff = now - startStamp,
-				    msLeft = jQuery.cookie("idleCountdown");
-				clearTimeout(idleTimeoutID);
-				msLeft -= msDiff;
-				jQuery.cookie("idleCountdown",msLeft, {path: '/' });
-			});
-
-			function idlePopup() {	
-				jQuery.cookie("idlePagesLeft",null, {path: '/' });
-				jQuery.cookie("idleCountdown",null, {path: '/' });
-				jQuery.cookie("idleStamp",null, {path: '/' });
-				jQuery.cookie("idlePopped",true, {expires: 14, path: '/' });
-				jQuery("#openBroPop").fancybox({
-					maxWidth	: 660,
-					maxHeight	: 660,
-					fitToView	: false,
-					width		: '100%',
-					height		: 'auto',
-					autoSize	: false,
-					closeClick	: false,
-					openEffect	: 'none',
-					closeEffect	: 'none',
-					closeBtn : false,
-					scrolling : 'no' });
-				jQuery('#openBroPop').trigger('click');
-				jQuery('.close_popup_link').on("click", function(e) {
-				    jQuery.fancybox.close(true) });
-				return false;
-			}});
-		
-	</script>
-	<div class="triggerlink" style="display:none !important"><a href="#broPop" id="openBroPop" accesskey="P"></a></div>
-	<div id="broPop" class="aPop" style="display: none;">
-		<div class="wpb_row vc_row-fluid">
-			<div class="vc_span12 wpb_column">
-				<div class="close_popup"><a href="#" class="close_popup_link">CLOSE X</a></div>
-			</div>
-		</div>	
-		<div class="wpb_row vc_row-fluid">
-			<div class="vc_span6 wpb_column popup_content">
-				<h2>Discover why a Jacuzzi<sup>®</sup>&nbsp;Bathtub is the right choice</h2>
-				<ul>
-					<li>See inside the structure of a Jacuzzi<sup>®</sup>&nbsp;Bathtub</li>
-					<li>Compare features across models</li>
-					<li>Explore inspirational photos</li>
-				</ul>
-				<a href="http://www.jacuzzi.com/baths/support/brochure-request/" class="vc_btn vc_btn_grey vc_btn_md vc_btn_square_outlined vc_btn_blue">Get Your Brochure</a>
-			</div>
-			<div class="vc_span6 wpb_column popup_img">
-				<img src="<?php bloginfo('template_url'); ?>/images/brochure-cover.jpg" class="popup_img" />
-			</div>
-		</div>
-	</div>
-<?php 	
-}
-
 /* Its more effective to create template for Walk in page. But dont have wp-login so doing conditional check for this page. */
 if(is_page( 'walk-in' ))
 {
@@ -143,10 +53,12 @@ if(is_page( 'walk-in' ))
 		</div>	
 		<div class="wpb_row vc_row-fluid">
 			<div class="vc_span6 wpb_column popup_content">
-				<h2>WALK-IN TUBS<br/>FOR EXISTING HOMES</h2>
-				<p>The safer, more luxurious alternative to a traditional bath. Made to fit your existing bath space without any major remodeling required.</p>
-				<p>Limited Lifetime Warranty</p>
+				<h2>The Jacuzzi<sup>®</sup> Walk-In Tub<br>
+                  <small>Remodel &amp; Replacement</small><br>
+                  <small>Walk-In Bathtub</small><br></h2>
+				<p>Blends safety and hydromassage to keep you healthy and happy in your home. Designed to fit in place of a standard bathtub. Also comes in compact and large sizes. Lifetime warranty.</p>
 				<a href="http://www.hydrotherapybathing.com/about-walk-in-tubs/" class="vc_btn vc_btn_grey vc_btn_md vc_btn_square_outlined vc_btn_blue">LEARN MORE</a>
+				<p style="margin-top:13px;"><small>DIY or installing yourself? <a href="/baths/bathtubs/finestra-bath/">Click here</a>.</small></p>
 			</div>
 			<div class="vc_span6 wpb_column popup_img">
 				<img src="<?php bloginfo('template_url'); ?>/images/walkin_img.jpg" class="popup_img" />
